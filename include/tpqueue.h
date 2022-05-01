a2aca5fdb39ea1c1d68c35b3a1227516b41aaec5
GoncharenkoK <99185069+GoncharEkaterina@users.noreply.github.com>
2022-05-01 23:37:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,55 +1,57 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <iostream>
+#include <string>
 
 template<typename T>
 class TPQueue {
  private:
- TPQueue::ITEM*create(const T &data) {
- ITEM *item = new ITEM;
- item->data = data;
- item->next = nullptr;
- return item;
-}
- int *head;
- int *tail;
- struct ITEM {
-  T data;
-  ITEM * next;
-  ITEM * prev;
+  int *head;
+  int *tail;
+  struct ITEM {
+    T data;
+    ITEM * next;
+    ITEM * prev;
  };
+  TPQueue::ITEM*create(const T &data) {
+  ITEM *item = new ITEM;
+  item->data = data;
+  item->next = nullptr;
+  return item;
+}
  
  public:
- T pop() {
- if (head) {
-  ITEM *temp = head->next;
-  T data = head->data;
-  delete head;
-  head = temp;
-  return data;
- } else { throw std::string("Empty"); }
+  T pop() {
+    if (head) {
+      ITEM *temp = head->next;
+      T data = head->data;
+      delete head;
+      head = temp;
+      return data;
+    } else { throw std::string("Empty"); }
  }
- T push(const T &data) {
-  ITEM *temp = head;
-  ITEM *item = create(data);
-  while (temp && temp->prior.data >= data.prior) {
-   tail = tail->next;
-   if (!temp && head) {
-    tail->next = item;
-    tail->next->prev = tail;
-    tail = item;
-   } else if (!temp && !head) { 
-    head = item = tail;
-   } else if (!temp->prev) {
-    temp->prev = item;
-    item->next = temp;
-    head = item;
-   } else {
-    temp->prev->next = item;
-    item->next = temp;
-    item->prev = item;
+  void push(const T &data) {
+    ITEM *temp = head;
+    ITEM *item = create(data);
+    while (temp && temp->prior.data >= data.prior) {
+      tail = tail->next;
+    if (!temp && head) {
+      tail->next = item;
+      tail->next->prev = tail;
+      tail = item;
+    } else if (!temp && !head) { 
+      head = item = tail;
+    } else if (!temp->prev) {
+      temp->prev = item;
+      item->next = temp;
+      head = item;
+    } else {
+      temp->prev->next = item;
+      item->next = temp;
+      item->prev = item;
+    }
   }
- }
 };
 
 struct SYM {
